cf0000f8359c023be11f0c2246a1cdd84534d997
xtrica <xtrica.com@gmail.com>
2018-01-24 13:43:13 -0500

    v1.0.1516819393

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /*
- * xtrica-connection v1.0.1516819247 (https://xtrica.com)
+ * xtrica-connection v1.0.1516819393 (https://xtrica.com)
  * Copyright 2017-2018 (or 2150?) Xtrica
  * Licensed under MIT
  */
-"use strict";export default function(){this.status=!1;this.mutex=!1;this.changed=!1;this.interval=null;this.setStatus=(value)=>{console.log('navigator detects: '+(value?'online':'offline'));let vm=this;vm.changed=!0;vm.status=value?!0:!1};this.setStatusOffline=()=>{console.log('window detects: offline');let vm=this;vm.changed=!0;vm.status=!1};this.setStatusOnline=()=>{console.log('window detects: online');let vm=this;vm.changed=!0;vm.status=!0};this.cleanup=()=>{let vm=this
+"use strict";export default function(){this.status=!0;this.mutex=!1;this.changed=!1;this.interval=null;this.setStatus=(value)=>{console.log('navigator detects: '+(value?'online':'offline'));let vm=this;vm.changed=!0;vm.status=value?!0:!1};this.setStatusOffline=()=>{console.log('window detects: offline');let vm=this;vm.changed=!0;vm.status=!1};this.setStatusOnline=()=>{console.log('window detects: online');let vm=this;vm.changed=!0;vm.status=!0};this.cleanup=()=>{let vm=this
 clearInterval(vm.interval);window.removeEventListener('online',vm.setStatusOnline);window.removeEventListener('offline',vm.setStatusOffline);if(!!navigator&&typeof navigator==='object'&&navigator.hasOwnProperty('onLine')){navigator.removeEventListener('onLine',vm.setStatus)}};if(!!navigator&&typeof navigator==='object'&&navigator.hasOwnProperty('onLine')){navigator.addEventListener('onLine',this.setStatus)}
 window.addEventListener('offline',this.setStatusOffline);window.addEventListener('online',this.setStatusOnline);this.interval=setInterval(()=>{let vm=this
 if(vm.changed===!1){if(window.location.hostname!=='localhost'){if(vm.mutex===!1){vm.mutex=!0;try{let x=new XMLHttpRequest();x.open('HEAD','/?time='+(new Date()).getTime());let timeout=setTimeout(()=>{console.log('request aborted');x.abort();vm.mutex=!1},2200);x.onreadystatechange=()=>{if(x.readyState===4){clearTimeout(timeout);console.log('request detects: '+(parseInt(x.status)===200?'online':'offline'));vm.status=parseInt(x.status)===200?!0:!1;vm.mutex=!1}};x.send(null)}catch(error){vm.mutex=!1}}}}else{vm.changed=!1}},1100)}
