d0cbf644158f85c53f4617f8c17946190ce4f27b
xtrica <xtrica.com@gmail.com>
2019-03-07 12:41:32 -0500

    v1.0.1551980492

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 /*
- * xtrica-connection v1.0.1520438302 (https://xtrica.com)
- * Copyright 2017-2018 (or 2150?) Xtrica
+ * xtrica-connection v1.0.1551980492 (https://xtrica.com)
+ * Copyright 2017-2019 (or 2150?) Xtrica
  * Licensed under MIT
  */
 "use strict";export default function(filepath){this.filepath=((!!filepath&&typeof filepath==="string"&&filepath.length)?filepath:"/");this.status=!0;this.statusChangedTo=!0;this.statusChangedConfirmations=0;this.mutex=!1;this.interval=null;this.changed=!1;this.setStatus=(value)=>{let vm=this;vm.changed=!0;vm.status=value?!0:!1};this.setStatusOffline=()=>{let vm=this;vm.changed=!0;vm.status=!1};this.setStatusOnline=()=>{let vm=this;vm.changed=!0;vm.status=!0};this.cleanup=()=>{let vm=this
-if(window.location.hostname==="localhost"){window.removeEventListener("online",vm.setStatusOnline);window.removeEventListener("offline",vm.setStatusOffline);if(!!navigator&&typeof navigator==="object"&&navigator.hasOwnProperty("onLine")){navigator.removeEventListener("onLine",vm.setStatus)}}else{clearInterval(vm.interval)}};if(window.location.hostname==="localhost"){if(!!navigator&&typeof navigator==="object"&&navigator.hasOwnProperty("onLine")){navigator.addEventListener("onLine",this.setStatus)}
-window.addEventListener("offline",this.setStatusOffline);window.addEventListener("online",this.setStatusOnline)}else{this.interval=setInterval(()=>{let vm=this
-if(vm.mutex===!1){vm.mutex=!0;try{let x=new XMLHttpRequest();x.open("HEAD",vm.filepath+"?connection="+((new Date()).toISOString()));let timeout=setTimeout(()=>{x.abort();vm.mutex=!1},450);x.onreadystatechange=()=>{if(x.readyState===4){clearTimeout(timeout);let newStatus=(((parseInt(x.status)>=200&&parseInt(x.status)<300)||parseInt(x.status)===304)?!0:!1);if(vm.statusChangedTo===newStatus){vm.statusChangedConfirmations+=1;if(vm.statusChangedConfirmations>=3){vm.status=((vm.statusChangedTo===!0)?!0:!1)}}else{vm.statusChangedTo=((newStatus===!0)?!0:!1);vm.statusChangedConfirmations=0}
-vm.mutex=!1}};x.send(null)}catch(error){vm.mutex=!1}}},500)}}
+window.removeEventListener("online",vm.setStatusOnline);window.removeEventListener("offline",vm.setStatusOffline)};window.addEventListener("offline",this.setStatusOffline);window.addEventListener("online",this.setStatusOnline)}
